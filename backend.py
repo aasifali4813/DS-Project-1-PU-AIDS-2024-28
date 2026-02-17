2733284c5cb519c58fa4a6237f2281e7dc27b797
aimlrl <mlresearch.axis@gmail.com>
2026-02-17 09:17:05 +0530

    Completed backend as well as frontend

--- backend.py
+++ backend.py
@@ -1,11 +1,14 @@
 import numpy as np
-import scipy.stats as s
 import pickle
+import scipy.stats as s
 import config
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
-from fastapi import Query
 from typing import Annotated
+
+def compute_normalizing_probability(placement_equals_0_likelihood, placement_equals_1_likelihood):
+    return (placement_equals_0_likelihood*(1-config.PLACEMENT_EQUALS_1_PRIOR)) +\
+    (placement_equals_1_likelihood*config.PLACEMENT_EQUALS_1_PRIOR)
 
 def determine_placement_posterior_probability(input_features):
 
@@ -20,7 +23,7 @@ def determine_placement_posterior_probability(input_features):
     with open("likelihood_distribution_params.pkl","rb") as file_handle:
         likelihood_distribution_params = pickle.load(file_handle)
 
-    for input_feat, input_feat_value in zip(config.INPUT_FEATURE_NAMES, new_input_features):
+    for input_feat, input_feat_value in zip(config.NEW_DATA_COLUMN_NAMES, new_input_features):
         mu_0, sigma_0 = likelihood_distribution_params[0][input_feat]
         mu_1, sigma_1 = likelihood_distribution_params[1][input_feat]
         
@@ -30,36 +33,46 @@ def determine_placement_posterior_probability(input_features):
         placement_equals_0_likelihood = placement_equals_0_likelihood * p_input_feature_on_0_placement
         placement_equals_1_likelihood = placement_equals_1_likelihood * p_input_feature_on_1_placement
 
-    placement_equals_0_posterior = placement_equals_0_likelihood * (1-config.PLACEMENT_EQUALS_1_PRIOR)
+    placement_equals_0_posterior = placement_equals_0_likelihood * (1 - config.PLACEMENT_EQUALS_1_PRIOR)
     placement_equals_1_posterior = placement_equals_1_likelihood * config.PLACEMENT_EQUALS_1_PRIOR
+
+    normalizing_probability = compute_normalizing_probability(placement_equals_0_likelihood,
+                                                              placement_equals_1_likelihood)
+    placement_equals_0_posterior = placement_equals_0_posterior/normalizing_probability
+    placement_equals_1_posterior = placement_equals_1_posterior/normalizing_probability
 
     if placement_equals_1_posterior[0] > placement_equals_0_posterior[0]:
         return {"result":"Given your inputs, most likely you are going to get placed and the probability of you getting placed is roughly {}".format(placement_equals_1_posterior[0])}
     else:
         return {"result":"Given your inputs, most likely you are not going to get placed and the probability of you getting placed is roughly {}".format(placement_equals_1_posterior[0])}
-    
+
 
 app = FastAPI()
 
 
 class InputFeatureVector(BaseModel):
-
     iq:Annotated[int, Query(ge=40,le=160)]
     previous_semester_result:Annotated[float, Query(ge=0,le=10)]
     cgpa:Annotated[float, Query(ge=0,le=10)]
     communication_skills:Annotated[int, Query(ge=0,le=10)]
     projects_completed:Annotated[int, Query(ge=0,le=10)]
 
+
 @app.get("/")
 def home_page():
 
-    return "This web App predicts the Probability of a Student being Placed based on five factors"
-    
-    
+    return "This Web App predicts the probability of a student being placed based on five factors"
+
+
 @app.post("/compute-probability")
 def compute_probability(input_features:InputFeatureVector):
-    input_feature_values_list = list()
+
+    input_features_values_list = list()
+
     for input_feature_name, input_feature_value in InputFeatureVector.model_fields.items():
-        input_feature_values_list.append(getattr(input_features,input_feature_name))
-    response = determine_placement_posterior_probability(input_feature_values_list)
+        input_features_values_list.append(getattr(input_features,input_feature_name))
+
+    response = determine_placement_posterior_probability(input_features_values_list)
     return response
+
+
--- frontend.py
+++ frontend.py
@@ -1,34 +1,37 @@
 import streamlit as st
 import requests
-import config
-
 
 def compute_probability(payload):
+
     r = requests.post(url="http://127.0.0.1:8080/compute-probability",json=payload)
     return r.json()
 
-
 st.set_page_config(page_title="A Web App",layout="centered")
 st.title("Determine Placement Probability")
-st.caption("Frontend: Streamlit | Backend: REST API")
+st.caption("Frontend:Streamlit | Backend: REST API")
 st.subheader(body="Please enter your details below",text_alignment="center")
 
+iq = st.number_input("Enter your IQ here",min_value=40,max_value=160,
+                     value="min",step=1)
+previous_semester_result = st.number_input("Enter your Previous Semester Result",
+                                           min_value=0.0,max_value=10.0,value="min",
+                                           step=0.01)
+cgpa = st.number_input("Enter your CGPA",min_value=0.0,max_value=10.0,value="min",
+                                           step=0.01)
+communication_skills = st.number_input("Enter your Communication Skills",
+                                       min_value=0,max_value=10,
+                                       value="min",step=1)
+projects_completed = st.number_input("Enter your Number of Completed Projects",
+                                       min_value=0,max_value=10,
+                                       value="min",step=1)
 
-iq = st.number_input(label="Enter your IQ here",min_value=40,max_value=160,value="min",step=1)
-previous_semester_result = st.number_input(label="Enter your Previous Semester GPA",min_value=0.0,
-                                           max_value=10.0,value="min",step=0.01)
-cgpa = st.number_input(label="Enter your overall CGPA till current semester",min_value=0.0,
-                       max_value=10.0,value="min",step=0.01)
-
-communication_skills = st.number_input(label="How much you rate yourself on the scale of 10 in terms of Communication Skills",
-                                       min_value=0,max_value=10,value="min",step=1)
-projects_completed = st.number_input(label="Enter how many projects you have completed so far",
-                                     min_value=0,max_value=10,value=0,step=1)
-payload = {"iq":iq, "previous_semester_result":previous_semester_result, "cgpa":cgpa,
-           "communication_skills":communication_skills,"projects_completed":projects_completed}
+payload = {"iq":iq, "previous_semester_result":previous_semester_result,
+           "cgpa":cgpa,"communication_skills":communication_skills,
+           "projects_completed":projects_completed}
 
 text_area_placeholder = st.empty()
 
 if st.button("Calculate Probability"):
     response = compute_probability(payload)
-    text_area_placeholder.text_area(label="Your Placement Probability",value=response["result"])
+    text_area_placeholder.text_area(label="Your Placement Probability",
+                                    value=response["result"])
